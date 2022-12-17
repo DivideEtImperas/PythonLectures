c493259c048d7d8b26a9379948a21631a362bf6b
Eduard <tula71ya@gmail.com>
2022-12-17 23:06:53 +0300

    создан файл log.txt

---
+++ Workout/demo.py
@@ -0,0 +1,39 @@
+# Вычисление расстояния и расхода топлива
+# dist = 0  # Расстояние которое нужно проехать
+# speed = 0  # Средняя скорость авто, км/ч
+# consum = 0  # Средний расход 10.5 л/100 км
+#
+# dist = int(input("Расстояние: "))
+# speed = int(input("Планируемая средняя скорость: "))
+# consum = float(input("Средний расход топливаа л/100 км: "))
+# time = dist * 60 / speed
+# result = consum * dist / 100
+# print("Время в пути ", time, "минут.")
+# print("Необходимо ", result, "л.")
+
+# Стоимость содержания автомобиля
+
+# service = float(input("Стоимость ТО: "))
+# fuel = float(input("Стоимость топлива: "))
+# tax = float(input("Транспортный налог: "))
+# tuning = float(input("Тюнинг и прочие доработки: "))
+# insurance = float(input("ОСАГО: "))
+#
+# total = service + fuel + tax + tuning + insurance
+#
+# print("Всего: ", total)
+
+
+# print("Hello, "+ "world!")
+# print([1,2,3] + [4,5,6])
+# print((1,2)+(3,4))
+# print((1,2)*3)
+#
+# print("s" in "String")
+# print("s" in "string")
+# print(3 in [1,2,3])
+# print(5 in (1,2,3))
+
+a = 8
+
+print(bin(a))
